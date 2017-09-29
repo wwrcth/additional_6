db33d7cab2b43fea3b4102f8a483683304fadd20
Nestserava Katsiaryna <wwrcth@gmail.com>
2017-09-29 14:43:35 +0300

    Updated algorithm

--- src/index.js
+++ src/index.js
@@ -20,69 +20,59 @@ module.exports = function zeros(expression) {
     }
   })
 
-  var zeroNumbers = 0;
-  var res = 1;
+  var fifthfNumbers = 0;
+  var secondNumbers = 0;
+
   for (var i = 0; i < numberArr.length; i++) {
     if (factorialArr[i] === 1) {
-      for (var j = 5; j <= numberArr[i]; j = j + 5) {
-        zeroNumbers++;
-        if (j == 25 || j == 50 || j == 75 || j == 100) {
-          zeroNumbers++;
+      for (var j = 1; j <= numberArr[i]; j++) {
+        var number = j;
+        while (number % 2 === 0) {
+          if (number % 2 === 0) {
+            number = number / 2;
+            secondNumbers++;
+          }
+        }
+        while (number % 5 === 0) {
+          if (number % 5 === 0) {
+            number = number / 5;
+            fifthfNumbers++;
+          }
         }
       }
     } else {
-      res = multiply(res, factorial(numberArr[i]));
+      if (numberArr[i] % 2 === 0) {
+        for (var j = 2; j <= numberArr[i]; j = j + 2) {
+          var number = j;
+          while (number % 2 === 0) {
+            if (number % 2 === 0) {
+              number = number / 2;
+              secondNumbers++;
+            }
+          }
+          while (number % 5 === 0) {
+            if (number % 5 === 0) {
+              number = number / 5;
+              fifthfNumbers++;
+            }
+          }
+        }
+      } else {
+        for (var j = 1; j <= numberArr[i]; j = j + 2) {
+          var number = j;
+          while (number % 5 === 0) {
+            if (number % 5 === 0) {
+              number = number / 5;
+              fifthfNumbers++;
+            }
+          }
+        }
+      }
     }
   }
-  var j = res.length - 1;
-  while (res[j] === '0' && j !== 0) {
-    if (res[j] === '0') {
-      zeroNumbers++;
-    }
-    j--;
+  if (fifthfNumbers < secondNumbers) {
+    return fifthfNumbers;
+  } else {
+    return secondNumbers;
   }
-  return zeroNumbers;
-}
-
-function factorial(n) {
-  return (n >= 2) ? multiply(n, factorial(n - 2)) : 1;
-}
-
-function multiply(a, b) {
-  var first = a.toString();
-  var second = b.toString();
-  if (first.length < second.length) {
-    var temp = second;
-    second = first;
-    first = temp;
-  }
-  var a1 = first.split('');
-  var a2 = second.split('');
-  var arr = [];
-  var rlen = a1.length + a2.length;
-  var i, j;
-
-  for (i = 0; i < rlen; i++) {
-    arr[i] = 0;
-  }
-
-  for (i = 0; i < a2.length; i++) {
-    for (j = 0; j < a1.length; j++) {
-      arr[1 + j + i] += a1[j] * a2[i];
-    }
-  }
-
-  for (i = rlen - 1; i > 0; i--) {
-    if (arr[i] >= 10) {
-      arr[i - 1] += Math.floor(arr[i] / 10);
-      arr[i] %= 10;
-    }
-  }
-
-  if (arr[0] == 0) {
-    delete arr[0];
-  }
-
-
-  return arr.join('');
 }
